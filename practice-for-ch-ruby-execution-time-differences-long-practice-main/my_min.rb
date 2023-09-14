69a56ebdedfc90241c278280955c0445ff2b6c28
Gary <g@modernhuman.xyz>
2023-09-14 13:48:28 -0400

    Finish largest_contiguous_subsum. Progress on faster_largest_contiguous_subsum

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/my_min.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/my_min.rb
@@ -24,8 +24,8 @@ def largest_contiguous_subsum(list) # O(n^2)
     combos = []
 
     (0...list.length).each do |i| 
-        (i+1...list.length).each do |j|
-            combos << [list[i], list[j]] # O(n^2)
+        (i...list.length).each do |j|
+            combos << list[i..j] # O(n^2)
         end
     end
 
@@ -33,13 +33,27 @@ def largest_contiguous_subsum(list) # O(n^2)
     sums.max # O(n)
 end # O(n^2) + O(2n) #=> drop less-dominant becomes #=> O(n^2)
 
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list) # => 8 (14 wrong)
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (-4 wrong)
+
+
+
+
 def faster_largest_contiguous_subsum(list) # O(n)
     largest_sum = list.first
-    (0...list.length-1).each do |i|
-        largest_sum = list[i] + list[i+1] if list[i] + list[i+1] > largest_sum
+    (1...list.length-1).each do |i|
+        if list[i] + list[i..-i].sum > largest_sum
+            largest_sum = list[i] + list[i..-i].sum
+        end
     end
     largest_sum
 end
+
+
 
 list = [5, 3, -7]
 p faster_largest_contiguous_subsum(list) # => 8
