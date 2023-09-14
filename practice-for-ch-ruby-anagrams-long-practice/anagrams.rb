bd02ca27525cd24a8ad4cf3bc04043405fac5f0c
claudiaaziz <claudiahannahh@gmail.com>
2023-09-14 16:32:17 -0400

    Finished Anagrams

--- practice-for-ch-ruby-anagrams-long-practice/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice/anagrams.rb
@@ -1,7 +1,7 @@
-def first_anagram?(string1, string2)
+def first_anagram?(string1, string2) # O(n^n)
     anagrams = []
 
-    until anagrams.length == string1.length ** string1.length #O(n^2)
+    until anagrams.length == string1.length ** string1.length 
         anagrams << string1.chars.shuffle.join("") #
     end
 
@@ -14,10 +14,10 @@ end
 # p first_anagram?("GARY", "YRAG")
 # p first_anagram?("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ZYXWVUTSRQPONMLKJIHGFEDCBA")
 
-def second_anagram?(string1, string2)
+def second_anagram?(string1, string2) # O(n)
     delete_me = string2.split("")
   (0...string1.length).each do |i|
-    index = delete_me.find_index(string1[i])
+    index = delete_me.find_index(string1[i]) 
     delete_me.delete_at(index) if !index.nil?
   end
   delete_me.empty?
@@ -28,4 +28,24 @@ end
 # p second_anagram?("CLAUDIA", "DIACLAU")
 # p second_anagram?("GARY", "YERRRRR")
 
+def third_anagram?(string1, string2) # O(n log n)
+    string1.chars.sort == string2.chars.sort
+end
 
+# p third_anagram?("APPLE", "PEPLA")
+# p third_anagram?("RUBY", "YUBR")
+# p third_anagram?("CLAUDIA", "DIACLAU")
+# p third_anagram?("GARY", "YERRRRR")
+
+def fourth_anagram?(string1, string2) # O(n)
+    str1_hash = Hash.new(0)
+    str2_hash = Hash.new(0)
+    string1.each_char { |char| str1_hash[char] += 1 }
+    string2.each_char { |char| str2_hash[char] += 1 }
+    str1_hash == str2_hash
+end
+
+p fourth_anagram?("APPLE", "PEPLA")
+p fourth_anagram?("RUBY", "YUBR")
+p fourth_anagram?("CLAUDIA", "DIACLAU")
+p fourth_anagram?("GARY", "YERRRRR")
