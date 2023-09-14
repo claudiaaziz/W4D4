3e9284e38f1a5f1f05b42071a417d6aef61afdf1
Gary <g@modernhuman.xyz>
2023-09-14 15:54:59 -0400

    Finish second_anagram?

--- practice-for-ch-ruby-anagrams-long-practice/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice/anagrams.rb
@@ -1,15 +1,31 @@
 def first_anagram?(string1, string2)
     anagrams = []
 
-    until anagrams.length == string1.length ** string1.length
-        anagrams << string1.chars.shuffle.join("")
+    until anagrams.length == string1.length ** string1.length #O(n^2)
+        anagrams << string1.chars.shuffle.join("") #
     end
 
-    anagrams.include?(string2)
+    anagrams.uniq.include?(string2)
+    # anagrams
 end
 
-
 # p first_anagram?("APPLE", "PEPLA")
-# p first_anagram?("RUBY", "YUBRZ")
+# p first_anagram?("RUBY", "YUBR")
 # p first_anagram?("GARY", "YRAG")
-p first_anagram?("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ZYXWVUTSRQPONMLKJIHGFEDCBA")
+# p first_anagram?("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ZYXWVUTSRQPONMLKJIHGFEDCBA")
+
+def second_anagram?(string1, string2)
+    delete_me = string2.split("")
+  (0...string1.length).each do |i|
+    index = delete_me.find_index(string1[i])
+    delete_me.delete_at(index) if !index.nil?
+  end
+  delete_me.empty?
+end
+
+# p second_anagram?("APPLE", "PEPLA")
+# p second_anagram?("RUBY", "YUBR")
+# p second_anagram?("CLAUDIA", "DIACLAU")
+# p second_anagram?("GARY", "YERRRRR")
+
+
